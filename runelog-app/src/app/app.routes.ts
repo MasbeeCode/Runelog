12ca9d1f186baf3478738fda0c32ac5d13711426
Mason <masonhillsoftware@gmail.com>
2025-03-08 22:59:17 -0700

    Add Bootstrap & Started User Overview Component

--- runelog-app/src/app/app.routes.ts
+++ runelog-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppUserOverviewComponent } from './components/app-user-overview/app-user-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '**', component: AppUserOverviewComponent,
+        children: [
+        ]
+    },
+];
