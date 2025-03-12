5adaaebbd52e12b4b2c27dd07f8801f7059f8ff3
Mason <masonhillsoftware@gmail.com>
2025-03-12 15:43:20 -0600

    Disabled SSR/Prerender

--- runelog-app/src/app/app.routes.ts
+++ runelog-app/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AppUserOverviewComponent } from './components/app-user-overview/app-user-overview.component';
+import { AppFeatOverviewComponent } from './components/app-feat-overview/app-feat-overview.component';
 
 export const routes: Routes = [
     {
-        path: '**', component: AppUserOverviewComponent,
-        children: [
-        ]
+        path: '', redirectTo: '/user', pathMatch: 'full',
     },
+    {
+        path: 'user', component: AppUserOverviewComponent
+    },
+    {
+        path: 'test', component: AppFeatOverviewComponent
+    }
 ];
